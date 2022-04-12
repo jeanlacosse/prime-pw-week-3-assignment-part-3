2b76d38dd2db7e8727458f5bc55ab294b7adaa57
jeanlacosse <jeanlacosse@gmail.com>
2022-04-12 11:08:00 -0500

    Finished all original goals

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -2,21 +2,25 @@ console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
+let partsNeeded = 40;
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+console.log('1. Number of partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
+let supplyChanges = [3, 5, -6, 0, 7, 11]
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+console.log('2. Array of supplyChanges:', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+supplyChanges.pop()
+console.log('4. Removed item:', supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
+supplyChanges.push(25);
 console.log('5. Adding 25 to supplyChanges.');
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
@@ -24,6 +28,17 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
+
+for (x in supplyChanges) {
+  console.log(supplyChanges[x]);
+  if (supplyChanges[x] > 0) {
+    console.log (`Added ${supplyChanges[x]} parts.`)
+  } else if (supplyChanges[x] === 0) {
+    console.log (`No change.`)
+  } else (supplyChanges[x] < 0) {
+    console.log (`Removed ${supplyChanges[x]} parts.`)
+  } 
+}
 console.log('6. Showing supplyChanges...');
 
 
